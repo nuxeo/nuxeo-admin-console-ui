91c1a3d26e217903d97e454972a748cff86b85ce
arupmmi07 <arupmmi07@gmail.com>
2024-03-08 02:02:22 +0530

    todo: comments added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // This is the only difference between the development and production environments
+  // Ttodo: his is the only difference between the development and production environments
   // demo url: https://api.realworld.io/api
   apiUrl: 'https://api.realworld.io/api',
 }
