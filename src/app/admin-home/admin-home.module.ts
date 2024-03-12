b91c7368a794a685bee4635d11058a4d83716e43
arupmmi07 <arupmmi07@gmail.com>
2024-03-12 13:25:46 +0530

    integrated hylandui software, configurations, refactored code & added modules

---
+++ src/app/admin-home/admin-home.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AdminHomeComponent } from "./components/admin-home.component";
+
+@NgModule({
+  declarations: [AdminHomeComponent],
+  imports: [CommonModule],
+})
+export class AdminHomeModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
-import { AdminVideoRenderGenerationModule } from "./features/admin-video-render-generation/admin-video-render-generation.module";
-import { AdminPictureRenderGenerationModule } from "./features/admin-picture-render-generation/admin-picture-render-generation.module";
-import { AdminThumbnailGenerationModule } from "./features/admin-thumbnail-generation/admin-thumbnail-generation.module";
-import { AdminFullTextReindexModule } from "./features/admin-full-text-reindex/admin-full-text-reindex.module";
-import { AdminElasticSearchReindexModule } from "./features/admin-elastic-search-reindex/admin-elastic-search-reindex.module";
-import { AdminBulkActionMonitoringModule } from "./features/admin-bulk-action-monitoring/admin-bulk-action-monitoring.module";
-import { AdminHomeModule } from "./features/admin-home/admin-home.module";
-import { CommonModule } from "@angular/common";
-import { AdminWarningComponent } from "./shared/components/admin-warning/admin-warning.component";
+import { AdminHomeModule } from "./admin-home/admin-home.module";
+import { AdminWarningComponent } from "./adminWarning/admin-warning.component";
+import { AdminMenuComponent } from "./adminMenu/admin-menu.component";
+import { AdminHeaderComponent } from "./adminHeader/admin-header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthRoutingModule } from "./auth/auth-routing.module";
 import { HylandSSOManagerComponent } from "./auth/components/SSO/hylandSSOManager.component";
@@ -17,19 +12,24 @@ import { AuthInterceptorService } from "./auth/services/auth-interceptor.service
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-import { StoreRouterConnectingModule, routerReducer } from "@ngrx/router-store";
 import { StoreModule } from "@ngrx/store";
+import { StoreRouterConnectingModule, routerReducer } from "@ngrx/router-store";
 import { authReducer } from "./auth/store/reducers";
 import { EffectsModule } from "@ngrx/effects";
 import * as authEffects from "./auth/store/effects";
 import { ngrxDevtools } from "../devtools/ngrx-devtools";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSidenavModule } from "@angular/material/sidenav";
-import { AdminHeaderComponent } from "./layouts/admin-header/admin-header.component";
-import { AdminMenuComponent } from "./layouts/admin-menu/admin-menu.component";
-import { AdminSystemInformationModule } from "./features/admin-system-information/admin-system-information.module";
+import { AdminSystemInformationModule } from "./admin-system-information/admin-system-information.module";
+import { AdminBulkActionMonitoringModule } from "./admin-bulk-action-monitoring/admin-bulk-action-monitoring.module";
+import { AdminElasticSearchReindexModule } from "./admin-elastic-search-reindex/admin-elastic-search-reindex.module";
+import { AdminFullTextReindexModule } from "./admin-full-text-reindex/admin-full-text-reindex.module";
+import { AdminThumbnailGenerationModule } from "./admin-thumbnail-generation/admin-thumbnail-generation.module";
+import { AdminPictureRenderGenerationModule } from "./admin-picture-render-generation/admin-picture-render-generation.module";
+import { AdminVideoRenderGenerationModule } from "./admin-video-render-generation/admin-video-render-generation.module";
 
 @NgModule({
   declarations: [
