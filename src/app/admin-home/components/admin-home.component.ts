5740b989623ae8ecf19cb6490ab3464df2135edf
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2024-04-25 17:10:34 +0530

    NXP-32250: admin console warning on launch

--- src/app/admin-home/components/admin-home.component.ts
+++ src/app/admin-home/components/admin-home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from "@angular/core";
+import { MatDialog } from '@angular/material/dialog';
+import { AdminWarningComponent } from "src/app/adminWarning/admin-warning.component";
+
 
 @Component({
   selector: "admin-home",
   templateUrl: "./admin-home.component.html",
 })
 export class AdminHomeComponent {
-  constructor() {}
+  constructor(private dialogService: MatDialog) {}
 
   ngOnInit() {
     console.log("From Home !");
-  }
+    this.dialogService.open(AdminWarningComponent, {
+      data: 'HELLO',
+    });
+}
 }
