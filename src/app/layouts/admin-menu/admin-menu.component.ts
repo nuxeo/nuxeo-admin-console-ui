ee27fcafd015975cd5e5c23202b8c326cf922a45
srakeshkumar1019 <srakeshkumar1019@gmail.com>
2024-05-13 15:41:36 +0530

    NXP-32255: add side navigation bar

--- src/app/layouts/admin-menu/admin-menu.component.ts
+++ src/app/layouts/admin-menu/admin-menu.component.ts
@@ -1,7 +1,18 @@
 import { Component } from "@angular/core";
-
+import { NAV_ITEMS, NavItem } from "./admin-menu.constants";
 @Component({
   selector: "admin-menu",
   templateUrl: "./admin-menu.component.html",
+  styleUrls: ['./admin-menu.component.scss']
 })
-export class AdminMenuComponent {}
+export class AdminMenuComponent {
+  constructor() { }
+  navItems: NavItem[] = NAV_ITEMS;
+  onClickSelectItem(id: number): void {
+    this.navItems = this.navItems.map(item => ({
+      ...item,
+      isSelected: item.id === id
+    }));
+    const selectedItem = this.navItems.find(item => item.id === id);
+  }
+}
