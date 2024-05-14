2735c19dc63211987d8c10a26f2d611056bddd61
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2024-05-14 12:46:53 +0530

    NXP-32250: admin console warning on launch

--- src/app/admin-app.component.ts
+++ src/app/admin-app.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { authActions } from "./auth/store/actions";
+import { MatDialog } from "@angular/material/dialog";
+import { AdminWarningComponent } from "./shared/components/admin-warning/admin-warning.component";
 
 @Component({
   selector: "admin-app-root",
   templateUrl: "./admin-app.component.html",
 })
 export class AdminAppComponent implements OnInit {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private dialogService: MatDialog) {}
 
   ngOnInit(): void {
     this.store.dispatch(authActions.getCurrentUser());
+    const preference = localStorage.getItem("doNotWarn");
+    if (preference === "true") {
+    } else {
+      this.dialogService.open(AdminWarningComponent, {
+        disableClose: true, // Disable closing on clicking outside
+      });
+    }
   }
 }
