c7d29bdaf5e408398fafcc6fb92cd60fcfd72755
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2024-06-13 00:41:52 +0530

    NXP-32275: admin console confirmation request

--- src/app/shared/components/reindex/reindex-confirmation-modal/reindex-confirmation-modal.component.ts
+++ src/app/shared/components/reindex/reindex-confirmation-modal/reindex-confirmation-modal.component.ts
@@ -1,8 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: "reindex-confirmation-modal",
   templateUrl: "./reindex-confirmation-modal.component.html",
   styleUrls: ["./reindex-confirmation-modal.component.scss"],
 })
-export class ReindexConfirmationModalComponent {}
+export class ReindexConfirmationModalComponent {
+
+  constructor(
+    private dialogRef: MatDialogRef<ReindexConfirmationModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  confirm() {
+    this.dialogRef.close(true);
+  }
+
+  cancel() {
+    this.dialogRef.close(false);
+  }
+
+  copyActionId() {
+    navigator.clipboard.writeText(this.data.actionId).then(() => {
+      alert('Action ID copied to clipboard!');
+    });
+  }
+}
