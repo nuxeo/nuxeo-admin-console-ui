60e55f2a677947c392cc36d0a52c03c8e9c523fa
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2024-06-25 14:20:06 +0530

    nishant ui changes

--- src/app/features/elastic-search-reindex/components/elastic-search-reindex.component.ts
+++ src/app/features/elastic-search-reindex/components/elastic-search-reindex.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { ElasticSearchType } from '../elastic-search-reindex.interface';
-import { ELASTIC_SEARCH_REINDEX_TYPES } from '../elastic-search-reindex.constants';
-import { ElasticSearchReindexService } from '../services/elastic-search-reindex.service';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from "@angular/core";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { filter, takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { ElasticSearchType } from "../elastic-search-reindex.interface";
+import { ELASTIC_SEARCH_REINDEX_TYPES } from "../elastic-search-reindex.constants";
+import { ElasticSearchReindexService } from "../services/elastic-search-reindex.service";
 
 @Component({
-  selector: 'elastic-search-reindex',
-  templateUrl: './elastic-search-reindex.component.html',
-  styleUrls: ['./elastic-search-reindex.component.scss']
+  selector: "elastic-search-reindex",
+  templateUrl: "./elastic-search-reindex.component.html",
+  styleUrls: ["./elastic-search-reindex.component.scss"],
 })
 export class ElasticSearchReindexComponent implements OnInit, OnDestroy {
   searchTabs: ElasticSearchType[] = ELASTIC_SEARCH_REINDEX_TYPES;
   activeTab: ElasticSearchType = this.searchTabs[0];
-  pageTitle = '';
+  pageTitle = "";
   private ngUnsubscribe = new Subject<void>(); // Subject to unsubscribe from observables
 
   constructor(
@@ -25,19 +25,25 @@ export class ElasticSearchReindexComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      takeUntil(this.ngUnsubscribe) // Unsubscribe when component is destroyed
-    ).subscribe(() => {
-      this.updateActiveTab();
-    });
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.ngUnsubscribe) // Unsubscribe when component is destroyed
+      )
+      .subscribe(() => {
+        this.updateActiveTab();
+      });
 
-    this.elasticSearchReindexService.pageTitle.pipe(
-      takeUntil(this.ngUnsubscribe) // Unsubscribe when component is destroyed
-    ).subscribe((title) => {
-      this.pageTitle = title;
-      this.cdRef.detectChanges(); // Trigger change detection manually
-    });
+    this.elasticSearchReindexService.pageTitle
+      .pipe(
+        takeUntil(this.ngUnsubscribe) // Unsubscribe when component is destroyed
+      )
+      .subscribe((title) => {
+        this.pageTitle = title;
+        this.cdRef.detectChanges(); // Trigger change detection manually
+      });
+
+    this.updateActiveTab(); // Ensure the active tab is set on component init
   }
 
   ngOnDestroy() {
@@ -48,7 +54,9 @@ export class ElasticSearchReindexComponent implements OnInit, OnDestroy {
   updateActiveTab() {
     const currentRoute = this.route.snapshot.firstChild?.routeConfig?.path;
     if (currentRoute) {
-      this.activeTab = this.searchTabs.find(tab => tab.path === currentRoute) || this.searchTabs[0];
+      this.activeTab =
+        this.searchTabs.find((tab) => tab.path === currentRoute) ||
+        this.searchTabs[0];
     }
   }
 
