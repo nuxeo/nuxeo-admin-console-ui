70e18ffd98146de489391ccb801a08baa8dfaefe
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2024-07-03 14:27:03 +0530

    NXP-32262: elasticsearch reindex nxql query

--- nuxeo-admin-console-web/angular-app/src/app/features/elastic-search-reindex/store/reducers.ts
+++ nuxeo-admin-console-web/angular-app/src/app/features/elastic-search-reindex/store/reducers.ts
@@ -30,6 +30,20 @@ export const initialFolderReindexState: FolderReindexState = {
   error: null,
 };
 
+export interface NXQLReindexState {
+  nxqlReindexInfo: {
+    commandId: string | null;
+  };
+  error: HttpErrorResponse | null;
+}
+
+export const initialNXQLReindexState: NXQLReindexState = {
+  nxqlReindexInfo: {
+    commandId: null,
+  },
+  error: null,
+};
+
 export const reindexReducer = createReducer(
   initialDocumentState,
   on(ReindexActions.performDocumentReindex, (state) => ({
@@ -74,6 +88,31 @@ export const folderReindexReducer = createReducer(
   on(ReindexActions.resetFolderReindexState, (state) => ({
     ...state,
     folderReindexInfo: {
+      commandId: null,
+    },
+    error: null,
+  }))
+);
+
+export const nxqlReindexReducer = createReducer(
+  initialNXQLReindexState,
+  on(ReindexActions.performNxqlReindex, (state) => ({
+    ...state,
+    error: null,
+  })),
+  on(ReindexActions.onNxqlReindexLaunch, (state, { nxqlReindexInfo }) => ({
+    ...state,
+    nxqlReindexInfo: {
+      commandId: nxqlReindexInfo?.commandId,
+    },
+  })),
+  on(ReindexActions.onNxqlReindexFailure, (state, { error }) => ({
+    ...state,
+    error,
+  })),
+  on(ReindexActions.resetNxqlReindexState, (state) => ({
+    ...state,
+    nxqlReindexInfo: {
       commandId: null,
     },
     error: null,
