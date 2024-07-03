c8ccff0670f35bb95413f727e4f3d9802ad96631
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2024-07-03 13:12:20 +0530

    Modified constants

--- nuxeo-admin-console-web/angular-app/src/app/features/elastic-search-reindex/elastic-search-reindex.constants.ts
+++ nuxeo-admin-console-web/angular-app/src/app/features/elastic-search-reindex/elastic-search-reindex.constants.ts
@@ -2,8 +2,8 @@ import { ElasticSearchType } from "./elastic-search-reindex.interface";
 
 export const ELASTIC_SEARCH_REINDEX_TYPES: ElasticSearchType[] = [
   { label: "Document", path: "document", isSelected: true },
-  { label: "Folder", path: "folder", isSelected: false},
-  { label: "NXQL Query", path: "nxql", isSelected: false }
+  { label: "Folder", path: "folder", isSelected: false },
+  { label: "NXQL Query", path: "nxql", isSelected: false },
 ];
 
 export const ELASTIC_SEARCH_REINDEX_MODAL_DIMENSIONS = {
@@ -14,10 +14,15 @@ export const ELASTIC_SEARCH_REINDEX_MODAL_DIMENSIONS = {
 export const ELASTIC_SEARCH_REINDEX_TABS_TITLE = {
   DOCUMENT: "Reindex a single document",
   FOLDER: "Reindex a document and all of its children",
-  NXQL_QUERY: "Reindex the results of a NXQL query"
-}
+  NXQL_QUERY: "Reindex the results of a NXQL query",
+};
 
 export const ELASTIC_SEARCH_LABELS = {
+  MODAL_TYPE: {
+    confirm: 1,
+    launched: 2,
+    error: 3,
+  },
   INVALID_DOCID_OR_PATH_ERROR:
     "Please provide a document id or a document path",
   INVALID_DOCID_ERROR: "Please provide a document id",
@@ -42,11 +47,6 @@ export const ELASTIC_SEARCH_LABELS = {
   COPY_ACTION_ID_BUTTON_LABEL: "Copy action ID",
   IMPACT_MESSAGE:
     "The query could impact performance if it involves high volumes. Documents will not be available for search while reindexing is in progress.",
-  MODAL_TYPE: {
-    confirm: 1,
-    launched: 2,
-    error: 3,
-  },
   REFERENCE_POINT: 2000,
   NO_DOCUMENTS: "There are no documents to be reindexed.",
   DOCUMENT_ID_OR_PATH: "Document ID or Path",
@@ -54,5 +54,5 @@ export const ELASTIC_SEARCH_LABELS = {
   REINDEX_BUTTON_LABEL: "Reindex",
   ACTION_ID_COPIED_ALERT: "Action ID copied to clipboard!",
   SELECT_BASE_QUERY: "SELECT * FROM Document WHERE",
-  UNEXPECTED_ERROR:"Unexpected error format"
+  UNEXPECTED_ERROR: "Unexpected error format",
 };
