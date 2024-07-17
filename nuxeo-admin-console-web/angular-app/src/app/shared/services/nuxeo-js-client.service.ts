9360b9b9042641e0def1ae5bca65e15e78ab4f35
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2024-07-17 16:14:19 +0530

    NXP-32772: handle urls (#104)
    * NXP-32772: handle urls
    
    * NXP-32772: handle constants labels

--- nuxeo-admin-console-web/angular-app/src/app/shared/services/nuxeo-js-client.service.ts
+++ nuxeo-admin-console-web/angular-app/src/app/shared/services/nuxeo-js-client.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from "@angular/core";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Nuxeo from "nuxeo";
+type URL = string | null;
 
 @Injectable({
   providedIn: "root",
 })
+
 export class NuxeoJSClientService {
   nuxeoInstance: Nuxeo = {};
+  private _removeSlashFromURL(url:URL):URL{
+    return  url?.endsWith('/') ? url?.slice(0,-1) : url;
+  }
   initiateJSClient(url: string | null): Nuxeo {
     let configObj: object | null = null;
     if(url) {
@@ -16,12 +21,14 @@ export class NuxeoJSClientService {
     this.nuxeoInstance = new Nuxeo(configObj);
   }
 
-  getBaseUrl(): string {
-    return this.nuxeoInstance["_baseURL"];
+  getBaseUrl(): URL {
+    const baseURL=  this.nuxeoInstance["_baseURL"];
+    return this._removeSlashFromURL(baseURL);
   }
 
-  getApiUrl(): string {
-    return this.nuxeoInstance["_restURL"];
+  getApiUrl(): URL {
+    const restURL =  this.nuxeoInstance["_restURL"];
+    return this._removeSlashFromURL(restURL);
   }
 
   getNuxeoInstance(): Nuxeo {
