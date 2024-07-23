5c7f121773c23f7b05bd346ab9b7cd0eb5b63d2e
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2024-07-23 12:55:13 +0530

    NXP-32730: add unit test for logout & username (#116)
    NXP-32730: add unit test for logout & username

--- nuxeo-admin-console-web/angular-app/src/app/app.component.spec.ts
+++ nuxeo-admin-console-web/angular-app/src/app/app.component.spec.ts
@@ -1,22 +1,50 @@
 import { MatDialogModule } from "@angular/material/dialog";
-import { BaseLayoutComponent } from "./layouts/base-layout/components/base-layout.component";
 import { AppComponent } from "./app.component";
-import {
-  ComponentFixture,
-  ComponentFixtureAutoDetect,
-  TestBed,
-} from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { CommonModule } from "@angular/common";
 import { PersistenceService } from "./shared/services/persistence.service";
 import { WarningComponent } from "./features/warning/warning.component";
 import { CommonService } from "./shared/services/common.service";
 import { EventEmitter } from "@angular/core";
-import { provideMockStore } from '@ngrx/store/testing';
-import { StoreModule } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { AuthStateInterface } from './auth/types/authState.interface';
+import { UserInterface } from './shared/types/user.interface';
+import { APP_CONSTANTS } from './app.constants';
+import { By } from '@angular/platform-browser';
+import { authActions } from './auth/store/actions';
+import { ActivatedRoute } from '@angular/router';
+import { BaseLayoutModule } from "./layouts/base-layout/base-layout.module";
+import { BaseLayoutComponent } from "./layouts/base-layout/components/base-layout.component"
+import { HyMaterialIconModule } from "@hyland/ui";
 
 describe("AppComponent", () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: {
+        get: () => 'Administrator'
+      }
+    }
+  };
+
+  let store: MockStore<{ auth: AuthStateInterface }>;
+  const initialAuthState: AuthStateInterface = {
+    isSubmitting: false,
+    currentUser: {
+      id: 'Administrator',
+      isAdministrator: true,
+      properties: {
+        firstName: "nco",
+        lastName: "admin",
+        email: "nco-admin@nuxeo.com",
+        username: "Administrator"
+      }
+    } as UserInterface,
+    isLoading: false,
+    validationErrors: null
+  };
+
   class persistenceServiceStub {
     get() {
       return null;
@@ -31,75 +59,109 @@ describe("AppComponent", () => {
     loadApp = new EventEmitter<boolean>();
   }
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent, BaseLayoutComponent],
-      imports: [CommonModule, MatDialogModule, StoreModule.forRoot(provideMockStore)],
+      imports: [CommonModule, MatDialogModule, BaseLayoutModule, HyMaterialIconModule],
       providers: [
-        { provide: ComponentFixtureAutoDetect, useValue: true },
         { provide: PersistenceService, useClass: persistenceServiceStub },
         { provide: CommonService, useClass: commonServiceStub },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        provideMockStore({ initialState: { auth: initialAuthState } }),
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
   });
 
   it("should test if component is created", () => {
     expect(component).toBeTruthy();
   });
 
-  describe("test warning preference", () => {
-    beforeEach(() => {
-      spyOn(component.dialogService, "open");
+  describe("ngOnInit", () => {
+    it("should initiate JS client and subscribe to currentUser$", () => {
+      spyOn(component['nuxeoJsClientService'], 'initiateJSClient');
+      spyOn(component.currentUser$, 'subscribe').and.callThrough();
+      component.ngOnInit();
+      expect(component['nuxeoJsClientService'].initiateJSClient).toHaveBeenCalledWith(component.baseUrl);
+      expect(component.currentUser$.subscribe).toHaveBeenCalled();
     });
+
     it("should open the warning dialog if warning preference is not set", () => {
-      if (component?.currentUser) {
+      spyOn(component.dialogService, "open");
       spyOn(component.persistenceService, "get").and.returnValue(null);
-      const loadAppSubscriptionSpy = spyOn(
-        component.commonService.loadApp,
-        "subscribe"
-      ).and.callThrough();
       component.ngOnInit();
       expect(component.persistenceService.get).toHaveBeenCalled();
-      expect(component.dialogService.open).toHaveBeenCalledWith(
-        WarningComponent,
-        {
-          disableClose: true,
-        }
-      );
-      expect(loadAppSubscriptionSpy).toHaveBeenCalled();
-    }
+      expect(component.dialogService.open).toHaveBeenCalledWith(WarningComponent, { disableClose: true });
     });
 
     it("should not open the warning dialog if warning preference is set", () => {
-      if (component?.currentUser) {
+      spyOn(component.dialogService, "open");
       spyOn(component.persistenceService, "get").and.returnValue("true");
       component.ngOnInit();
       expect(component.persistenceService.get).toHaveBeenCalled();
       expect(component.dialogService.open).not.toHaveBeenCalled();
-      expect(component.loadApp).toEqual(true);
-      }
+      expect(component.loadApp).toBe(true);
     });
   });
 
-  it("should set loadApp to true or false based on the value received from the service subscription", () => {
-    if (component?.currentUser) {
-    component.ngOnInit();
-    component.commonService.loadApp.emit(true);
-    expect(component.loadApp).toBe(true);
-    component.ngOnInit();
-    component.commonService.loadApp.emit(false);
-    expect(component.loadApp).toBe(false);
-    }
+  describe("loadAppSubscription", () => {
+    it("should set loadApp based on the value received from the service subscription", () => {
+      component.ngOnInit();
+      component.commonService.loadApp.emit(true);
+      expect(component.loadApp).toBe(true);
+      component.commonService.loadApp.emit(false);
+      expect(component.loadApp).toBe(false);
+    });
   });
 
-  it("should remove theloadAppSubscription when component is destroyed", () => {
-    if (component?.currentUser) {
-    spyOn(component.loadAppSubscription, "unsubscribe");
-    component.ngOnDestroy();
-    expect(component.loadAppSubscription.unsubscribe).toHaveBeenCalled();
-    }
+  describe("ngOnDestroy", () => {
+    it("should unsubscribe from subscriptions", () => {
+      spyOn(component.loadAppSubscription, "unsubscribe");
+      spyOn(component.currentUserSubscription, "unsubscribe");
+      component.ngOnDestroy();
+      expect(component.loadAppSubscription.unsubscribe).toHaveBeenCalled();
+      expect(component.currentUserSubscription.unsubscribe).toHaveBeenCalled();
+    });
+  });
+
+  describe("onSignOut", () => {
+    it("should dispatch signOut action", () => {
+      spyOn(store, "dispatch");
+      component.onSignOut();
+      expect(store.dispatch).toHaveBeenCalledWith(authActions.signOut());
+    });
+  });
+  
+
+  describe("DOM", () => {
+    it("should display unauthorized message if currentUser is not administrator", () => {
+      store.setState({
+        auth: {
+          ...initialAuthState, currentUser: {
+            id: 'Administrator', isAdministrator: false, properties: {
+              firstName: "nco",
+              lastName: "admin",
+              email: "nco-admin@nuxeo.com",
+              username: "Administrator"
+            }
+          }
+        }
+      });
+      fixture.detectChanges();
+      const unauthorizedMessage = fixture.debugElement.query(By.css('.unauthorized'));
+      expect(unauthorizedMessage).toBeTruthy();
+      expect(unauthorizedMessage.nativeElement.textContent).toContain(APP_CONSTANTS.UNAUTHORIZED_MESSAGE);
+    });
+
+    it("should display base-layout if loadApp is true and currentUser is administrator", () => {
+      component.loadApp = true;
+      fixture.detectChanges();
+      const baseLayout = fixture.debugElement.query(By.css('base-layout'));
+      expect(baseLayout).toBeTruthy();
+    });
   });
 });
--- nuxeo-admin-console-web/angular-app/src/app/features/warning/warning.component.spec.ts
+++ nuxeo-admin-console-web/angular-app/src/app/features/warning/warning.component.spec.ts
@@ -2,32 +2,39 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { HyDialogBoxModule } from "@hyland/ui";
 import { MatDialogModule } from "@angular/material/dialog";
 import { WarningComponent } from "./warning.component";
-import {
-  ComponentFixture,
-  ComponentFixtureAutoDetect,
-  TestBed,
-} from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { CommonModule } from "@angular/common";
 import { PersistenceService } from "../../shared/services/persistence.service";
 import { CommonService } from "../../shared/services/common.service";
 import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter } from "@angular/core";
-import { StoreModule } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 
 describe("WarningComponent", () => {
   let component: WarningComponent;
   let fixture: ComponentFixture<WarningComponent>;
-  class persistenceServiceStub {
+  let persistenceServiceSetSpy: jasmine.Spy;
+  let commonServiceLoadAppEmitSpy: jasmine.Spy;
+
+  const initialAuthState = {
+    auth: {
+      isSubmitting: false,
+      currentUser: { id: 'Administrator', isAdministrator: false, properties: {} },
+      isLoading: false,
+      validationErrors: null
+    }
+  };
+
+  class PersistenceServiceStub {
     get() {
       return null;
     }
 
     set() {
-      return;
+      return null;
     }
   }
 
-  class commonServiceStub {
+  class CommonServiceStub {
     loadApp = new EventEmitter<boolean>();
   }
 
@@ -39,71 +46,63 @@ describe("WarningComponent", () => {
         MatDialogModule,
         HyDialogBoxModule,
         MatCheckboxModule,
-        StoreModule.forRoot(provideMockStore)
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        { provide: ComponentFixtureAutoDetect, useValue: true },
-        { provide: PersistenceService, useClass: persistenceServiceStub },
-        { provide: CommonService, useClass: commonServiceStub },
+        { provide: PersistenceService, useClass: PersistenceServiceStub },
+        { provide: CommonService, useClass: CommonServiceStub },
+        provideMockStore({ initialState: initialAuthState }),
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WarningComponent);
     component = fixture.componentInstance;
+    persistenceServiceSetSpy = spyOn(component.persistenceService, "set");
+    commonServiceLoadAppEmitSpy = spyOn(component.commonService.loadApp, "emit");
   });
 
   it("should test if component is created", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should initialise the doNotWarn field based on preference saved", () => {
-    if (component?.currentUser) {
-      const persistenceServiceGetSpy = spyOn(component.persistenceService, "get");
-      persistenceServiceGetSpy.and.returnValue(null);
-      component.ngOnInit();
-      expect(component.doNotWarn).toBe(false);
-      persistenceServiceGetSpy.and.returnValue("true");
-      component.ngOnInit();
-      expect(component.doNotWarn).toBe(true);
-    }
-
+  it("should initialize the doNotWarn field based on preference saved", () => {
+    const persistenceServiceGetSpy = spyOn(component.persistenceService, "get");
+    persistenceServiceGetSpy.and.returnValue(null);
+    component.ngOnInit();
+    expect(component.doNotWarn).toBe(false);
+    persistenceServiceGetSpy.and.returnValue("true");
+    component.ngOnInit();
+    expect(component.doNotWarn).toBe(true);
   });
 
   describe("should test confirm click actions", () => {
-    let persistenceServiceSetSpy: jasmine.Spy;
-    let commonServiceLoadAppEmitSpy: jasmine.Spy;
     beforeEach(() => {
-      persistenceServiceSetSpy = spyOn(component.persistenceService, "set");
-      commonServiceLoadAppEmitSpy = spyOn(
-        component.commonService.loadApp,
-        "emit"
-      );
+      component.currentUser = {
+        id: 'Administrator', isAdministrator: false, properties: {
+          firstName: "nco",
+          lastName: "admin",
+          email: "nco-admin@nuxeo.com",
+          username: "Administrator"
+        }
+      };
     });
 
     it("should set preference as true & close dialog & emit loadApp=true, when doNotWarn field is checked", () => {
-      if (component?.currentUser) {
-        component.doNotWarn = true;
-        spyOn(component.dialogService, "closeAll");
-        component.onConfirm();
-        const preferenceKey = `doNotWarn-${component?.currentUser?.id}`;
-        expect(persistenceServiceSetSpy).toHaveBeenCalledWith(
-          preferenceKey,
-          "true"
-        );
-        expect(commonServiceLoadAppEmitSpy).toHaveBeenCalledWith(true);
-        expect(component.dialogService.closeAll).toHaveBeenCalled();
-      }
-
+      component.doNotWarn = true;
+      spyOn(component.dialogService, "closeAll");
+      component.onConfirm();
+      const preferenceKey = `doNotWarn-${component.currentUser?.id}`;
+      expect(persistenceServiceSetSpy).toHaveBeenCalledWith(preferenceKey, "true");
+      expect(commonServiceLoadAppEmitSpy).toHaveBeenCalledWith(true);
+      expect(component.dialogService.closeAll).toHaveBeenCalled();
     });
+
     it("should not set preference & close dialog & emit loadApp=true, when doNotWarn field is unchecked", () => {
-      if (component?.currentUser) {
-        component.doNotWarn = false;
-        spyOn(component.dialogService, "closeAll");
-        component.onConfirm();
-        expect(commonServiceLoadAppEmitSpy).toHaveBeenCalledWith(true);
-        expect(component.dialogService.closeAll).toHaveBeenCalled();
-      }
+      component.doNotWarn = false;
+      spyOn(component.dialogService, "closeAll");
+      component.onConfirm();
+      expect(commonServiceLoadAppEmitSpy).toHaveBeenCalledWith(true);
+      expect(component.dialogService.closeAll).toHaveBeenCalled();
     });
   });
 });
