51594568214613142df49e29bd4bf2bb689040ac
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2024-09-11 16:53:29 +0530

    removed unnecessary code

--- nuxeo-admin-console-web/angular-app/src/app/features/sub-features/probes-data/components/probes-data.component.ts
+++ nuxeo-admin-console-web/angular-app/src/app/features/sub-features/probes-data/components/probes-data.component.ts
@@ -1,4 +1,4 @@
-import { CommonService } from '../../../../shared/services/common.service';
+import { CommonService } from "../../../../shared/services/common.service";
 import { PROBES, PROBES_LABELS } from "../probes-data.constants";
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { Observable, Subscription } from "rxjs";
@@ -6,8 +6,8 @@ import { Store, select } from "@ngrx/store";
 import { ProbeState, ProbesInfo } from "../store/reducers";
 import * as ProbeActions from "../store/actions";
 import { ProbeDataService } from "../services/probes-data.service";
-import { HyToastService } from '@hyland/ui';
-import { HttpErrorResponse } from '@angular/common/http';
+import { HyToastService } from "@hyland/ui";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: "probes-data",
@@ -15,7 +15,7 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ["./probes-data.component.scss"],
 })
 export class ProbesDataComponent implements OnInit, OnDestroy {
-  @Input() summary = false; 
+  @Input() summary = false;
   probesData: ProbesInfo[] = [];
   fetchProbesSubscription = new Subscription();
   fetchProbes$: Observable<ProbesInfo[]>;
@@ -23,15 +23,27 @@ export class ProbesDataComponent implements OnInit, OnDestroy {
   columnsToDisplay: { propertyName: string; label: string }[] = [];
 
   defaultColumns = [
-    { propertyName: 'probe', label: 'Probe', summaryOnly: true },
-    { propertyName: 'success', label: 'Success', summaryOnly: true },
-    { propertyName: 'neverExecuted', label: 'Last Executed', summaryOnly: true },
-    { propertyName: 'information', label: 'Information', summaryOnly: true },
-    { propertyName: 'run', label: 'Run', summaryOnly: false },
-    { propertyName: 'successCount', label: 'Success Count', summaryOnly: false },
-    { propertyName: 'failureCount', label: 'Failure Count', summaryOnly: false },
-    { propertyName: 'time', label: 'Time', summaryOnly: false },
-    { propertyName: 'history', label: 'History', summaryOnly: false }
+    { propertyName: "probe", label: "Probe", summaryOnly: true },
+    { propertyName: "success", label: "Success", summaryOnly: true },
+    {
+      propertyName: "neverExecuted",
+      label: "Last Executed",
+      summaryOnly: true,
+    },
+    { propertyName: "information", label: "Information", summaryOnly: true },
+    { propertyName: "run", label: "Run", summaryOnly: false },
+    {
+      propertyName: "successCount",
+      label: "Success Count",
+      summaryOnly: false,
+    },
+    {
+      propertyName: "failureCount",
+      label: "Failure Count",
+      summaryOnly: false,
+    },
+    { propertyName: "time", label: "Time", summaryOnly: false },
+    { propertyName: "history", label: "History", summaryOnly: false },
   ];
   hideTitle = true;
   probeLaunchedSuccessSubscription = new Subscription();
@@ -46,7 +58,9 @@ export class ProbesDataComponent implements OnInit, OnDestroy {
     private commonService: CommonService,
     private toastService: HyToastService
   ) {
-    this.fetchProbes$ = this.store.pipe(select((state) => state.probes?.probesInfo));
+    this.fetchProbes$ = this.store.pipe(
+      select((state) => state.probes?.probesInfo)
+    );
     this.probeLaunchedSuccess$ = this.store.pipe(
       select((state) => state.probes?.probesInfo)
     );
@@ -55,22 +69,30 @@ export class ProbesDataComponent implements OnInit, OnDestroy {
     );
   }
   ngOnInit(): void {
-    this.columnsToDisplay = this.defaultColumns.filter(column => 
+    this.columnsToDisplay = this.defaultColumns.filter((column) =>
       this.summary ? column.summaryOnly : true
     );
-    this.hideTitle = !this.defaultColumns.some(col => col.summaryOnly && this.summary);
+    this.hideTitle = !this.defaultColumns.some(
+      (col) => col.summaryOnly && this.summary
+    );
 
-    this.fetchProbesSubscription = this.fetchProbes$.subscribe((data: ProbesInfo[]) => {
-      if (data?.length !== 0) {
-        this.probesData = data;
-      } else {
-        this.store.dispatch(ProbeActions.loadProbesData());
+    this.fetchProbesSubscription = this.fetchProbes$.subscribe(
+      (data: ProbesInfo[]) => {
+        if (data?.length !== 0) {
+          this.probesData = data;
+        } else {
+          this.store.dispatch(ProbeActions.loadProbesData());
+        }
       }
-    });
+    );
 
     this.probeLaunchedSuccessSubscription =
       this.probeLaunchedSuccess$.subscribe((data) => {
-        if (data && data?.length > 0 && Object.entries(this.probeLaunched).length > 0) {
+        if (
+          data &&
+          data?.length > 0 &&
+          Object.entries(this.probeLaunched).length > 0
+        ) {
           this.toastService.success(
             PROBES_LABELS.PROBE_LAUNCHED_SUCCESS.replaceAll(
               "{probeName}",
@@ -80,15 +102,6 @@ export class ProbesDataComponent implements OnInit, OnDestroy {
               canBeDismissed: true,
             }
           );
-         // const el = document.getElementsByClassName('hy-toast__dismiss');
-        //  console.log(el);
-          let button = document.querySelector('button.hy-toast__dismiss.mdc-icon-button.mat-mdc-icon-button');
-
-          // Check if the button exists and then focus on it
-          if (button instanceof HTMLButtonElement) {
-            button.tabIndex = 1;
-             // button.focus();
-          } 
         }
       });
 
@@ -128,7 +141,9 @@ export class ProbesDataComponent implements OnInit, OnDestroy {
   }
 
   isColumnVisible(propertyName: string): boolean {
-    return this.columnsToDisplay.some(column => column.propertyName === propertyName);
+    return this.columnsToDisplay.some(
+      (column) => column.propertyName === propertyName
+    );
   }
 
   viewDetails(): void {
