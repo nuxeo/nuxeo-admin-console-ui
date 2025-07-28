591fc90f1f48aba8c3e1fdee2c16b635ecae1035
naveen-konda <naveen.konda@hyland.com>
2025-07-28 17:48:01 +0530

    minor text changes to success message.

--- nuxeo-admin-console-web/angular-app/src/app/features/stream/stream.constants.ts
+++ nuxeo-admin-console-web/angular-app/src/app/features/stream/stream.constants.ts
@@ -63,8 +63,8 @@ export const CONSUMER_THREAD_POOL_LABELS = {
   CONSUMER_THREAD_POOL_OPERATION: "Consumer Thread Pool Operation",
   CONFIRMATION_MESSAGE: `This action will {operation} the consumer thread pool on all Nuxeo nodes :<br> <b> { id = '{selectedConsumerName}', urn = '{selectedConsumerUrn}' }. </b> <br> Would you like to continue?`,
   CONSUMER_THREAD_POOL_OPERATION_IN_PROGRESS_MSG: "An existing consumer thread pool operation is already in progress. Please wait until it completes.",
-  START_CONSUMER_SUCCESS_MSG: "Start consumer thread pool completed successfully.",
-  STOP_CONSUMER_SUCCESS_MSG: "Stop consumer thread pool completed successfully.",
+  START_CONSUMER_SUCCESS_MSG: "Consumer Thread Pool started successfully",
+  STOP_CONSUMER_SUCCESS_MSG: "Consumer Thread Pool stopped successfully",
   CONSUMER_THREAD_POOL_DIALOG_MSG: '{operation} Consumer Thread Pool',
   DIALOG_BOX_HEIGHT: '300px'
 };
