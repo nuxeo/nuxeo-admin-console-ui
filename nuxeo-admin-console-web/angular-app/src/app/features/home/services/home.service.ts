9bde0a0cdb2b39fdc34603eb9154ceb4dcfb0b45
naveen-konda <naveen.konda@hyland.com>
2025-08-11 10:09:56 +0530

    Removed unneccessary code

--- nuxeo-admin-console-web/angular-app/src/app/features/home/services/home.service.ts
+++ nuxeo-admin-console-web/angular-app/src/app/features/home/services/home.service.ts
@@ -3,17 +3,12 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { REST_END_POINTS } from "../../../shared/constants/rest-end-ponts.constants";
 import { NetworkService } from "../../../shared/services/network.service";
-import { MatDialogRef } from "@angular/material/dialog";
-import { ErrorModalComponent } from "../../sub-features/generic-multi-feature-layout/components/error-modal/error-modal.component";
-import { ErrorModalClosedInfo } from "src/app/shared/types/common.interface";
 import { InstanceInfo } from "../../../shared/types/instanceInfo.interface";
 
 @Injectable({
   providedIn: "root",
 })
 export class HomeService {
-  errorDialogRef: MatDialogRef<ErrorModalComponent, ErrorModalClosedInfo> =
-    {} as MatDialogRef<ErrorModalComponent, ErrorModalClosedInfo>;
 
   constructor(
     private networkService: NetworkService,
